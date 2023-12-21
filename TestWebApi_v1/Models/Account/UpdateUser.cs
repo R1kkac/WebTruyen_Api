9ec6c888a23d84588959e76e33a6f7d56f19a368
lesonhai0339 <haileds939@gmail.com>
2023-12-21 20:27:31 +0700

    up

--- TestWebApi_v1/Models/Account/UpdateUser.cs
+++ TestWebApi_v1/Models/Account/UpdateUser.cs
@@ -13,4 +13,16 @@ namespace TestWebApi_v1.Models.Account
         public string? Avatar { get; set; }
         public string? Name { get; set; }
     }
+    public class EditUser
+    {
+        public string email { get; set; }= null!;
+        public string? name { get;set; }
+        public string? phone { get; set; }
+    }
+    public class changePasswordUser
+    {
+        public string email { get; set; } = null!;
+        public string? oldpassword { get; set; } = null!;
+        public string? newpassword { get; set; } = null!;
+    }
 }
